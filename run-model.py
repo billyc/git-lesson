677be904dc3cffd07f69bfdfb52ee17626f44e5c
Billy Charlton <bcharlton@psrc.org>
2016-04-29 11:41:09 -0700

    initial commit

---
+++ run-model.py
@@ -0,0 +1,12 @@
+# Simple python script to "run the model" which really just
+# lists all the contributors
+
+import os
+
+files = os.listdir('contributors')
+
+# read first line of each file and print it
+for filename in files:
+	with open(os.path.join('contributors',filename)) as f:
+		print "Notes contributed by",f.readline()
+
